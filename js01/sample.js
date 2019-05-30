86a07ae0c04d14ab6bbb1b026126e5f3bae86be9
taekheonkim <48283125+taekheonkim@users.noreply.github.com>
2019-05-30 13:31:42 +0900

    Create sample.js

---
+++ js01/sample.js
@@ -0,0 +1,10 @@
+<html>
+<head></head>
+<body>
+<pre style="word-wrap: break-word; white-space: pre-wrap;">function fnTest(){
+document.write("자바스크립트환영!")
+alert("자바 스크립트 환영!!");
+}
+</pre>
+</body>
+</html>
